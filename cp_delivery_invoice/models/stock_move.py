7f7635634eedff11e9d51bf5fce2c1b6172d2f13
Mitul Dave <mitul.dave16@gmail.com>
2025-07-24 07:37:50 +0530

    [RMV] From main branch

--- cp_delivery_invoice/models/stock_move.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields
-
-
-class StockMove(models.Model):
-    _inherit = 'stock.move'
-
-    is_invoiced = fields.Boolean(string="Is Invoiced", default=False, copy=False)
--- mcp_odoo/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import chatbot_config
-from . import chatbot_message
-from . import chatbot_wizard 
-from . import anthropic_service 
